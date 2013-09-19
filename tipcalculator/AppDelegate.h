0818060329cbecfd59b3a5e264cf9fa4dbf3960e
Parikshit Tiwari <pari.future@gmail.com>
2013-09-18 23:30:07 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Parik Tiwari on 9/18/13.
+//  Copyright (c) 2013 DrunkDeveloper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
